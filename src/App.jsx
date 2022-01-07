1d9251b71ab9b2379237e7c40eb44fe38df98fec
Vishal2402 <70733643+Vishal2402@users.noreply.github.com>
2022-01-07 14:27:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,10 @@ const App = ()=>{
     <>
     <Navbar />
     <Switch>
-      <Route exact path="vishal2402.github.io/ReactAppBusiness/" component={Home} />
-      <Route exact path="vishal2402.github.io/ReactAppBusiness/about" component={About} />
-      <Route exact path="vishal2402.github.io/ReactAppBusiness/service" component={Service} /> 
-      <Route exact path="vishal2402.github.io/ReactAppBusiness/contact" component={Contact} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/service" component={Service} /> 
+      <Route exact path="/contact" component={Contact} />
       <Redirect to="/" />
       </Switch>
       <Footer />
